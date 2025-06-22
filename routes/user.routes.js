6a74203a3e12e300c6349093c3f027bd35f3ee87
koushikminfy <venkata.kowshik@minfytech.com>
2025-06-22 14:24:39 +0530

    first commit

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const router = express.Router();
-const { searchUser } = require('../controllers/user.controller');
+const { searchUser, getAllUsers } = require('../controllers/user.controller');
 
-router.get('/search', searchUser);
+const router = express.Router();
+
+// GET /api/users/search?email=test@example.com
+// GET /api/users/search?name=John
+router.get('/users/search', searchUser);
+
+// GET /api/users - get all users
+router.get('/users', getAllUsers);
 
 module.exports = router;
